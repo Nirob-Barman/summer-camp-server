972093bc6e68f45bf02f3306689952450b4f8f1b
Nirob Barman <niroborin111@gmail.com>
2023-06-12 15:10:06 +0600

    sending user data to the server

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xceqs5c.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +28,30 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+
+        const usersCollection = client.db("campDb").collection("users");
+
+
+        // create a collection with users
+
+        // app.get('/users', async (req, res) => {
+        //     const result = await usersCollection.find().toArray();
+        //     res.send(result);
+        // });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const query = { email: user.email }
+            const existingUser = await usersCollection.findOne(query);
+
+            if (existingUser) {
+                return res.send({ message: 'user already exists' })
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
 
 
 
