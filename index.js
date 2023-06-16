c756bc6591ab5537b767bfdd2e373c19f0967d62
Nirob Barman <niroborin111@gmail.com>
2023-06-16 10:59:07 +0600

    Vercel.json added

--- index.js
+++ index.js
@@ -111,14 +111,14 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/classes/:email', async (req, res) => {
-            const email = req.params.email
-            const query = { 'InstructorEmail': email }
-            const result = await classesCollection.find(query).toArray()
+        app.get("/classes/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { instructorEmail: email };
+            const result = await classesCollection.find(query).toArray();
 
-            console.log(result)
-            res.send(result)
-        })
+            console.log(result);
+            res.send(result);
+        });
 
         app.get("/classes/:id", async (req, res) => {
             const id = req.params.id;
